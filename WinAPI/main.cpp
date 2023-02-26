3af81f79dcf5c3a5f1f3219ad3645a9b30789dbd
Kremc_un30@top-academy.ru <Kremc_un30@top-academy.ru>
2023-02-26 14:47:30 +0300

    Add project files.

---
+++ WinAPI/main.cpp
@@ -0,0 +1,49 @@
+#include<Windows.h>
+#include"resource.h"
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+/*
+----------------------
+LP - Long Pointer
+STR - String
+LPSTR - Long pointer to string
+----------------------
+*/
+
+//#define MESSAGE_BOX
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+
+#ifdef MESSAGE_BOX
+	MessageBox(NULL, "Привет Мир!", "Hello", MB_YESNOCANCEL | MB_ICONINFORMATION | MB_HELP | MB_DEFBUTTON3 | MB_SYSTEMMODAL);
+	//NULL - родительское окно, NULL значит отсутствует
+	//"Привет Мир!" - сообщение
+	//"Hello" - заголовок окна
+	//MB_OK - Флаги, определяющие набор кнопок и значек окна
+	//Флаги - это биты, показывающие что включено, а что нет.  
+#endif // MESSAGE_BOX
+
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:	//Отрабатывает при запуске окна, и может создавать элементы окна
+		break;
+	case WM_COMMAND:	//Обрабатывает сообщения нажатия на кнопки и т.д.
+		switch (LOWORD(wParam))
+		{
+		case IDOK:		MessageBox(hwnd, "Была нажата кнопка OK", "Info", MB_OK | MB_ICONINFORMATION); break;
+		case IDCANCEL:	EndDialog(hwnd, 0); break;
+		}
+		break;
+	case WM_CLOSE:EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
